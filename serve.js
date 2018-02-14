fc2a42d28c1013bb752e034173a734218b263fe4
miffy24 <chenzhutao125@163.com>
2018-02-14 22:53:05 +0800

    image发送请求

--- serve.js
+++ serve.js
@@ -36,13 +36,17 @@ var server = http.createServer(function(request, response){
     response.setHeader('Content-Type', 'text/html; charset=utf-8')
     response.write(string)
     response.end()
-  }else if(path === '/pay'&&method.toUpperCase() === 'POST'){
+  }else if(path === '/pay'){
 	  var amount = fs.readFileSync('./db','utf8')
 	  var newAmount = amount - 1
 	  if(Math.random()>0.7){
 	     fs.writeFileSync('./db',newAmount)
-	     response.write('success')
+	     response.setHeader('Content-Type','image/jpg')
+	     response.statusCode = 200
+	     response.write(fs.readFileSync('./dog.jpg'))
+
 	  }else{
+	     response.statusCode = 400
 	     response.write('fail')
 	  }
 	  response.end()
